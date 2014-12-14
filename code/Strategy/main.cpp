15bf2f614a2c831952e8a4eec2e92504271f964d
me115 <me115@126.com>
2014-12-14 17:36:10 +0800

    将状态图文件改名
    将状态图文件改名，readthedocs 不支持中文命名；

--- code/Strategy/main.cpp
+++ code/Strategy/main.cpp
@@ -3,6 +3,7 @@
 #include "ConcreteStrategyA.h"
 #include "ConcreteStrategyB.h"
 #include "Strategy.h"
+#include <vector>
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -18,5 +19,21 @@ int main(int argc, char *argv[])
 	
 	delete s1;
 	delete s2;
+	
+	int rac1 = 0x1;
+	int rac2 = 0x2;
+	int rac3 = 0x4;
+	int rac4 = 0x8;
+	
+	int i = 0xe;
+	int j = 0x5;
+
+	int r1 = i & rac1;
+	int r2 = i & rac2;
+	int r3 = i & rac3;
+	int r4 = i & rac4;
+	
+	cout <<"res:" << r1 << "/" << r2 << "/" << r3 << "/" << r4 << endl;
+	
 	return 0;
 }
